103da23e07667d0746ad00a6461e875b6e0a7ed6
Bioline Alexandri <bioalexandri@gmail.com>
2025-02-13 15:33:52 +0700

    second commit

--- Assignment 2 - Library/Author.java
+++ Assignment 2 - Library/Author.java
@@ -3,6 +3,10 @@ public class Author{
     private String name;
     private String description;
 
+
+
+    
+
     public Author(String name, String description){
         this.name = name;
         this.description = description;
